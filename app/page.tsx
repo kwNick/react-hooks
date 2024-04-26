c46c1df3d66488511d79f38a69faee2092352a82
Nickolas Piraino <kwnp17c@gmail.com>
2024-04-26 15:29:22 -0400

    practicing with useEffect hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 'use client';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    try {
+      fetch('foo').then(() => { setLoaded(true); console.log("Loading") });
+
+    } catch (error) {
+      throw new Error("Error: fetch did not work: " + error);
+    }
+    // alert("Hello Side Effect!");
+    // return alert("Goodbye Side Effect!");
+    setTimeout(() => console.log("Set TimeOut for 3 seconds!", 3000));
+    setLoaded(false);
+  }, [count]);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="w-xl h-[50vh] z-10 w-full max-w-5xl items-center justify-center font-mono text-sm flex flex-col">
@@ -10,6 +23,7 @@ export default function Home() {
         <button onClick={() => setCount(count + 1)} className="border bg-rose-400 px-3 py-2 w-[24]">
           {count}
         </button>
+        {loaded ? <p>loading</p> : <h2>Home Page!!</h2>}
       </div>
     </main>
   );
